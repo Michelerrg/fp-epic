aad7f846a2e0e4792371ef4c342bf4f90783e047
Michelerrg <mhl.errigo@hotmail.it>
2022-04-20 15:23:42 +0200

    categories

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { getCategory } from "../../functions/category";
+import CategoryList from "../category-list/CategoryList";
+
+export default class Category extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categories: []
+        };
+    }
+
+    componentDidMount() {
+        getCategory().then(categories => this.setState({ categories }));
+    }
+
+    render() {
+        const list = this.state.categories.map(category => <CategoryList category={category} />);
+        
+        return (
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                { list }
+            </ul>
+        );
+    }
+    
+}
---
+++ src/functions/category.js
@@ -0,0 +1,8 @@
+import Category from "../models/Category";
+
+
+export const getCategory = () => {
+    return fetch('http://wordpress.test/wp-json/wp/v2/categories')
+    .then(resp => resp.json())
+    .then(categories => categories.map(category => new Category(category.id, category.name)));
+}
