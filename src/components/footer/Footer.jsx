e582a53f8eed34f165d6fba5cef2b9c617fa09e6
Michelerrg <mhl.errigo@hotmail.it>
2022-04-20 15:31:30 +0200

    header and footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+    render() {
+        return(
+            <footer className="mt-3 text-light bg-dark text-center px-5 pt-5">
+                <div className="row border-top border-2 border-secondary d-flex justify-content-between">
+                    <div className="col-lg-6 text-start pt-3">
+                        <p id="copyright"><small>&copy;Copyright by <b>Scrooge McDuck</b>. All Rights Reserved</small></p>
+                    </div>
+                    <div className="col-lg-6 text-end pt-3">
+                        <p><small>Designed by <span className="text-warning">Fidia</span></small></p>
+                    </div>
+                </div>
+	        </footer>
+        );
+    }
+}
